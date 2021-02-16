e888d1cb09109774a136efbff400245436d25471
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-02-15 21:50:53 -0600

    began styling, cannot get hover to work...

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className='container'>
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Resume</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav> 
+    )
+}
