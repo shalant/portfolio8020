124ea2ccc167cefe30abfa92729e88002d42a32a
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-02-16 21:37:53 -0600

    debugging, added css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar } from './components/Navbar';
+import Navbar from './components/Navbar';
+//import Header from './components/Header';
 
 function App() {
   return (
-    <Navbar >
-      <h2>Portfolio Project</h2>
-
-    </Navbar>
+    <>
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-export const Navbar = () => {
+const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className='container'>
+
                 <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+                    <FontAwesomeIcon icon={faBars} style={{ color: '#fff'}}/>
                 </button>
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item active">
@@ -33,3 +38,5 @@ export const Navbar = () => {
         </nav> 
     )
 }
+
+export default Navbar;
